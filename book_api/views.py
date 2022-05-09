b85d495ac488d2b831e7a307c0406ece48225fae
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-05-09 15:11:20 +0530

    Books api

--- book_api/views.py
+++ book_api/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from book_api.models import Book
+from django.http import JsonResponse
 
 
 # Create your views here.
 def book_list(request):
     books = Book.objects.all()
+    books_python = list(books.value())
+    return JsonResponse(
+        {
+            'books': books_python
+        }
+    )
