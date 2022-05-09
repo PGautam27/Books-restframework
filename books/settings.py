28057433b42bcaffc6cc235b2ee0e38e4a9240c4
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-05-09 15:45:07 +0530

    Books api

--- books/settings.py
+++ books/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'books.book_api.apps.BookApiConfig',
+    'book_api.apps.BookApiConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
