b95ef239163c5bb1c4f1adbe581b68e254f2ba56
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-05-09 15:24:30 +0530

    Books api

--- book_api/models.py
+++ book_api/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    title = models.CharField()
-    numberOfBooks = models.IntegerField()
-    published_date = models.IntegerField()
+    title = models.CharField(max_length=100)
+    number_of_pages = models.IntegerField()
+    publish_date = models.DateField()
     quantity = models.IntegerField()
 
     def __str__(self):
