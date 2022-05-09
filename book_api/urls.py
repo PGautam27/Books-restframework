864708ff72df7d8fce0986b5c8732bdc17d5bbba
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-05-09 17:41:13 +0530

    Books api

--- book_api/urls.py
+++ book_api/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from book_api.views import book_list, book_create, book
+from book_api.views import BookList, book_create, book
 urlpatterns = [
-    path('list/', book_list),
+    path('list/', BookList.as_view()),
     path('', book_create),
     path('<int:pk>', book)
 ]
