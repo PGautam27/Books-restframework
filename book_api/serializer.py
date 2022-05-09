dbcb316936e2a3a88d449d8aaf80e204e1425baf
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-05-09 16:54:11 +0530

    Books api

--- book_api/serializer.py
+++ book_api/serializer.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
+from book_api.models import Book
 
 
 class BookSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
+    id = serializers.IntegerField(read_only=True)
     title = serializers.CharField()
     number_of_pages = serializers.IntegerField()
     publish_date = serializers.DateField()
     quantity = serializers.IntegerField()
+
+    def create(self, data):
+        return Book.objects.create(**data)
+
+# {
+#    "title": "Harry Potter",
+#    "number_of_pages": 300,
+#    "publish_date": "2021-01-09",
+#    "quantity": 400
+# }
