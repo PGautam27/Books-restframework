7969bfa4b55ea498a5262222f7796c6367767dff
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-08-19 22:47:21 +0530

    Books api

--- book_api/models.py
+++ book_api/models.py
@@ -4,9 +4,9 @@ from django.db import models
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=100)
-    number_of_pages = models.IntegerField()
-    publish_date = models.DateField()
-    quantity = models.IntegerField()
+    author_name = models.CharField(max_length=100)
+    completed_reading = models.BooleanField()
+    reading_platform = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
--- book_api/serializer.py
+++ book_api/serializer.py
@@ -5,18 +5,18 @@ from book_api.models import Book
 class BookSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField()
-    number_of_pages = serializers.IntegerField()
-    publish_date = serializers.DateField()
-    quantity = serializers.IntegerField()
+    author_name = serializers.CharField(max_length=100)
+    completed_reading = serializers.BooleanField()
+    reading_platform = serializers.CharField(max_length=100)
 
     def create(self, data):
         return Book.objects.create(**data)
 
     def update(self, instance, data):
         instance.title = data.get('title', instance.title)
-        instance.number_of_pages = data.get('number_of_pages', instance.number_of_pages)
-        instance.publish_date = data.get('publish_date', instance.publish_date)
-        instance.quantity = data.get('quantity', instance.quantity)
+        instance.author_name = data.get('author_name', instance.author_name)
+        instance.completed_reading = data.get('completed_reading', instance.completed_reading)
+        instance.reading_platform = data.get('reading_platform', instance.reading_platform)
 
         instance.save()
         return instance
